82dd34bacb76fdc7a6b849a15dd2810f7dcdd768
oleg dashevskii <olegdashevskii@gmail.com>
2015-09-04 17:54:31 +0600

    Jemal.config

--- lib/jemal.rb
+++ lib/jemal.rb
@@ -24,21 +24,66 @@ module Jemal
   # Returns the String version.
   def self.version
     ptr = FFI::MemoryPointer.new :pointer
-    sizeptr = FFI::MemoryPointer.new :size_t
 
-    # FIXME wtf
-    case sizeptr.size
-    when 8
-      sizeptr.write_int64 ptr.size
-    when 4
-      sizeptr.write_int32 ptr.size
-    else
-      raise ArgumentError, "Unsupported architecture"
-    end
-
-    mallctl "version", ptr, sizeptr, nil, 0
+    mallctl "version", ptr, size_pointer(ptr), nil, 0
 
     strptr = ptr.read_pointer
     strptr.null? ? nil : strptr.read_string
+  end
+
+  CFG_PARAMS = %i(debug dss fill lazy_lock mremap munmap prof prof_libgcc
+                  prof_libunwind stats tcache tls utrace valgrind xmalloc)
+
+  # Public: Returns jemalloc config.
+  #
+  # Returns all config.* parameters in a single Hash.
+  #
+  # Examples
+  #
+  #   Jemal.config
+  #   # => {:debug=>false, :dss=>false, :fill=>true, :lazy_lock=>false, ... }
+  #
+  # Returns a Hash with Symbol keys and boolean values.
+  def self.config
+    CFG_PARAMS.inject({}) do |hash, param|
+      hash[param] = get_bool("config.#{param}")
+      hash
+    end
+  end
+
+  private
+
+  # Private: Use mallctl to read boolean value.
+  #
+  # name - the String with parameter name.
+  #
+  # Returns true or false.
+  def self.get_bool(name)
+    ptr = FFI::MemoryPointer.new :bool
+    mallctl name, ptr, size_pointer(ptr), nil, 0
+    ptr.get_uchar(0) > 0
+  end
+
+  # Private: Set up a size_t pointer.
+  #
+  # Creates a pointer to size_t value, which is set to
+  # baseptr.size.
+  #
+  # baseptr - a FFI::MemoryPointer instance.
+  #
+  # Returns a FFI::MemoryPointer.
+  def self.size_pointer(baseptr)
+    sizeptr = FFI::MemoryPointer.new :size_t
+
+    case sizeptr.size
+    when 8
+      sizeptr.write_int64 baseptr.size
+    when 4
+      sizeptr.write_int32 baseptr.size
+    else
+      raise ArgumentError, "Unsupported architecture: size_t size = #{sizeptr.size}"
+    end
+
+    sizeptr
   end
 end
--- test/jemal_test.rb
+++ test/jemal_test.rb
@@ -8,4 +8,16 @@ class JemalTest < Minitest::Test
   def test_that_it_returns_jemalloc_version
     assert_match /^\d+\.\d+\.\d+/, Jemal.version
   end
+
+  def test_config
+    cfg = Jemal.config
+
+    assert_kind_of Hash, cfg
+
+    assert_equal cfg.keys.sort, Jemal::CFG_PARAMS.sort
+
+    cfg.values.each do |v|
+      assert_includes [true, false], v
+    end
+  end
 end
